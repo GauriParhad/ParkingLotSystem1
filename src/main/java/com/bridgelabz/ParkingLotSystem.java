77dcab5e8fffda4519a5e495d3b9a27876ce7ca7
Gaurip10 <thegauriparhad@gmail.com>
2021-09-30 06:40:55 +0530

    Added: Creating Parking lot so that driver could able to catch their flight

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -1,2 +1,13 @@
-package com.bridgelabz;public class ParkingLotSystem {
+package com.bridgelabz;
+
+public class ParkingLotSystem {
+    public ParkingLotSystem(){
+    }
+
+    public boolean park(Object o) {
+        return true;
+    }
+
+
+
 }
--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -1,4 +1,15 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ParkingLotTest     {
+
+    @Test
+    public void givenVehicle_WhenParked_ShouldReturnTrue(){
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assertions.assertTrue(isParked);
+    }
+
 }
